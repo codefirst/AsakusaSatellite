059ecfc1a32ef22749d86b5d465cb512f3914c5a
suer <sue@nagoya-u.jp>
2011-01-16 22:55:57 +0900

    基本的なspecを追加 refs #213

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,7 +1,11 @@
 class ChatController < ApplicationController
   def tweet
-    puts Tweet
     @tweets = Tweet.all
   end
 
+  def room
+    if request.post?
+      Message.new(:body => params[:message]).save
+    end
+  end
 end

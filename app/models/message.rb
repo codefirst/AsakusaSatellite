642376db251d8344f3893f76db6492189f5bcc95
suer <sue@nagoya-u.jp>
2011-01-23 11:16:52 +0900

    お試し版(認証無し) refs #217

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveGroonga::Base
   def encode_json(_)
     { 
       'body' => self.body,
-      'name' => self.user.name,
+      'name' => (self.user ? self.user.name : 'Anonymous User'),
       'created_at' => self.created_at,
     }.to_json
   end

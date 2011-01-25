d933e765930e984ebbd6a05dfe637b5a49cde7ec
mzp <mzpppp@gmail.com>
2011-01-26 07:51:42 +0900

    Globalをやめて、jQueryプラグインとしてWebsocketChatを実装した refs #235

--- public/javascripts/room.js
+++ public/javascripts/room.js
@@ -1,62 +1,79 @@
-var config = {
-    entry : "ws://" + location.hostname + ":18081"
-}
-
-function log(msg){
-    console && console.log && console.log(msg);
-    $("div.status").html(msg);
-}
-
-function makeMessage(message){
-    return "<div class='message' target='" + message.id + "'>" +
-        "<p><img src='" + message.profile_image_url + "'>" +
-	"<span>" + message.name + "</span></p>" +
-	"<p>" + message.body + "</p></div>"
-}
-
-function appendMessage(message){
-    $(".message-list").append(makeMessage(message))
-}
-
-ws = new WebSocket(config.entry);
-ws.onopen = function() {
-    log("connected.");
-}
-
-ws.onmessage = function(text){
-    eval("var message = " + text.data);
-    if(message.event == "create"){
-	var c = message.content;
-	appendMessage(c);
+(function() {
+    function log(msg){
+	console && console.log && console.log(msg);
     }
-}
 
-ws.onerror = function(msg){
-    log("error: " + msg);
-}
+    jQuery.fn.recvMessage = function(config){
+	config = jQuery.extend({
+	    entry : "ws://" + location.hostname + ":18081"
+	},config);
 
-$(document).ready(function() {
-    $("form").bind("submit",function(e){
-	e.stopPropagation();
-	e.preventDefault();
+	var target = this;
 
-	var text = $("input.text", e.target).val();
+	function makeMessage(message){
+	    return "<div class='message' target='" + message.id + "'>" +
+		"<p><img src='" + message.profile_image_url + "'>" +
+		"<span>" + message.name + "</span></p>" +
+		"<p>" + message.body + "</p></div>"
+	}
 
-	jQuery.ajax({
-	    type: 'POST',
-	    url: '/api/v1/message',
-	    data: {
-		'room_id' : Global.room_id,
-		'message' : text
-	    },
-	    success: function(e) {
-		log("message sent:" + e);
-	    },
-	    error : function(e){
-		console.log(e);
-		log("message error:" + e.statusText);
+	function appendMessage(message) {
+	    target.append(makeMessage(message))
+	}
+
+	var ws = new WebSocket(config.entry);
+	ws.onopen = function() {
+	    log("connected.");
+	}
+
+
+	ws.onmessage = function(text){
+	    eval("var message = " + text.data);
+	    if(message.event == "create"){
+		var c = message.content;
+		appendMessage(c);
 	    }
-	});
+	}
+	ws.onerror = function(msg){
+	    log("error: " + msg);
+	}
 
-    });
-});
+    };
+
+    jQuery.fn.sendMessage = function(config){
+	config = jQuery.extend({
+	    url: '/api/v1/message',
+	    input : "input.text"
+	},config);
+	var target = this;
+
+	$().ready(function() {
+	    target.bind("submit",function(e){
+		e.stopPropagation();
+		e.preventDefault();
+
+		var elem = $(config.input, e.target);
+		var text = elem.val();
+		elem.val("");
+
+		jQuery.ajax({
+		    type: 'POST',
+		    url: config.url,
+		    data: {
+			'room_id' : config.room_id,
+			'message' : text
+		    },
+		    success: function(e) {
+
+			log("message sent:" + e);
+		    },
+		    error : function(e){
+			console.log(e);
+			log("message error:" + e.statusText);
+		    }
+		});
+	    });
+	});
+    };
+})(jQuery);
+

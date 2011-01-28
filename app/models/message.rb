161a72297adc0405c73e6a35702f97b2b0bf6c69
mzp <mzpppp@gmail.com>
2011-01-28 17:01:38 +0900

    メッセージの表示時にfilterを通すようにした refs #250

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,11 +3,16 @@ class Message < ActiveGroonga::Base
     {
       'id'   => self.id,
       'body' => self.body,
+      'html_body' => self.html_body,
       'name' => (self.user ? self.user.name : 'Anonymous User'),
       'screen_name' => (self.user ? self.user.screen_name : 'Anonymous User'),
       'profile_image_url' => (self.user ? self.user.profile_image_url : ''),
       'created_at' => self.created_at,
     }.to_json
+  end
+
+  def html_body
+    AsakusaSatellite::Filter.process self.body.to_s
   end
 end
 

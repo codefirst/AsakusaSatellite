746a12bcf59e51bdd0de5e4a96d9ab1a7c1e362f
suer <sue@nagoya-u.jp>
2011-01-31 08:04:44 +0900

    ページは見れる。復活の呪文が生成されない refs #282

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -5,7 +5,7 @@ class AccountController < ApplicationController
       return
     end 
 
-    if request.post? or not current_user.spell
+    if request.post? or current_user.spell.blank?
       length = (20..30).to_a.choice
       chars = ('a'..'Z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
       password = Array.new(length) { chars[rand(chars.size)] }.join

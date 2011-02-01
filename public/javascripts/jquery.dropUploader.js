5ff7ad4463874e1e63f35976c75c4b737cf59400
mzp <mzpppp@gmail.com>
2011-02-01 20:16:12 +0900

    create ページのテキストボックスの CSS が効かなくなったのを戻した refs #308

--- public/javascripts/jquery.dropUploader.js
+++ public/javascripts/jquery.dropUploader.js
@@ -43,7 +43,7 @@
             }
             else{
                 $(elm).css({
-                    "border" : "none"
+                    "border" : ""
                 });
             }
         }

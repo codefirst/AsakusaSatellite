19141e5ed77e1a55a3c7b40428be2245f54a0d67
mzp <mzpppp@gmail.com>
2011-02-02 07:42:27 +0900

    修正: Redmineリンクが別タブになっていない refs #314

--- vendor/plugins/as_redmine_ticket_link/lib/redmine_ticket_link.rb
+++ vendor/plugins/as_redmine_ticket_link/lib/redmine_ticket_link.rb
@@ -2,7 +2,7 @@ require 'uri'
 class AsakusaSatellite::Filter::RedmineTicketLink < AsakusaSatellite::Filter::Base
   def process(text)
     text.gsub(/#(\d+)/) do|ref|
-      %[<a href="#{config.roots}issues/#{$1}">#{ref}</a>]
+      %[<a target="_blank" href="#{config.roots}issues/#{$1}">#{ref}</a>]
     end
   end
 end

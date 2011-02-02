7a8a9ef2496bd0ccaf5cc0aebcdbf3d7131392d1
mzp <mzpppp@gmail.com>
2011-02-02 20:28:49 +0900

    twitterのユーザ名を抜きだす正規表現を修正 refs #317

--- vendor/plugins/as_twitter_link/lib/twitter_link.rb
+++ vendor/plugins/as_twitter_link/lib/twitter_link.rb
@@ -1,6 +1,6 @@
 class AsakusaSatellite::Filter::TwitterLink < AsakusaSatellite::Filter::Base
   def process(text)
-    text.gsub(/@(.+)/) do|ref|
+    text.gsub(/@([A-Za-z0-9_]+)/) do|ref|
       %[<a href="http://twitter.com/#{$1}" target="_blank">#{ref}</a>]
     end
   end

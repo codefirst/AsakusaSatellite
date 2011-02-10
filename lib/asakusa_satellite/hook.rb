39c968bd036bb4d2889771c3704f76cb52373d20
suer <sue@nagoya-u.jp>
2011-02-11 00:31:34 +0900

    タグがエスケープされてしまう問題を解決 refs #350

--- lib/asakusa_satellite/hook.rb
+++ lib/asakusa_satellite/hook.rb
@@ -3,19 +3,22 @@ module AsakusaSatellite::Hook
 
   class << self
     def add_listener(klass)
+      puts klass
       @@listeners << klass.new
     end
 
     def call_hook(hook, context = {})
+      puts @@listeners.size
       @@listeners.map do |listener|
         next unless listener.respond_to?(hook)
         listener.send(hook, context)
-      end
+      end.join {|elem| elem.class == String ? elem : ''}
     end
   end
 
   class Listener
     def self.inherited(klass)
+      puts klass
       AsakusaSatellite::Hook.add_listener(klass)
       super
     end

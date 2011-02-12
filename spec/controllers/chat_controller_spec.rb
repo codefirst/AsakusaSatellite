14bb9a6521cb491aba31432ef4a01921d094d55a
suer <sue@nagoya-u.jp>
2011-02-13 00:46:35 +0900

    chat controller の spec を追加 refs #338

--- spec/controllers/chat_controller_spec.rb
+++ spec/controllers/chat_controller_spec.rb
@@ -16,26 +16,35 @@ describe ChatController do
         post :message, {:room_id => room.id, :message => message}
       }.should change(Message.all.records, :size).by(1)
     end
-    it "部屋がない場合はエラーメッセージを表示する" do
-      pending
+
+    it "部屋がない場合はエラーメッセージとなる" do
       room = Room.new
       room.save
-      room.delete if room
-      post :message, {:room_id => room.id, :message => 'テストメッセージ'}
-      #response.should.redirect_to :error
+      room.delete
+      lambda {
+        post(:message, {:room_id => room.id, :message => 'テストメッセージ'})
+      }.should raise_error
     end
   end
 
   describe "発言一覧時は" do
-    it "デフォルトで該当する部屋のメッセージの1日分を取得する" do
-      pending
-      get :room, {:room_id => 1}
+    it "デフォルトで該当する部屋のメッセージの20件を取得する" do
+      room = Room.new
+      room.save
+      50.times do
+        Message.new(:room => room).save
+      end
+      get :room, {:id => room.id}
+      assigns[:messages].records.size.should == 20
     end
   end
 
   describe "発言更新時は" do
-    it "該当メッセージの更新日時が更新される" do
-      pending
+    it "該当メッセージの内容が更新される" do
+      message = Message.new(:body => 'init')
+      message.save
+      get :update_message_on_the_spot, {:id => message.id, :value => 'modified'}
+      Message.find(message.id).body.should == 'modified'
     end
   end
 

e5ac1262238df6175d863cb113e73273a015d39b
mallowlabs <mallowlabs@gmail.com>
2011-02-13 00:31:15 +0900

    Fixed: CRSF on deleting rooms refs #354

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -1,8 +1,10 @@
 class RoomController < ApplicationController
   def delete
-    room = Room.find(params[:id])
-    room.deleted = true
-    room.save
+    if request.post?
+      room = Room.find(params[:id])
+      room.deleted = true
+      room.save
+    end
     redirect_to :controller => 'chat', :action => 'index'
   end
 end

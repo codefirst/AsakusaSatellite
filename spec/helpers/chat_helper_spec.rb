db7112c564402913f14ce5dc022fce0c331a19cd
suer <sue@nagoya-u.jp>
2011-02-15 12:09:35 +0900

    message が保存できない場合の chat_helperのspecを追加 refs #342

--- spec/helpers/chat_helper_spec.rb
+++ spec/helpers/chat_helper_spec.rb
@@ -1,21 +1,48 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ChatHelper do
-  it "メッセージを作成する" do
-    room = Room.new
-    room.save
-    helper.create_message(room.id, 'message').body.should == 'message'
+  context "メッセージを作成する" do
+    it "メッセージの保存に成功する" do
+      room = Room.new
+      room.save
+      helper.create_message(room.id, 'message').body.should == 'message'
+    end
+    it "メッセージの保存に失敗する" do
+      message = mock
+      message.stub(:save).and_return(false)
+      Message.stub(:new).and_return(message)    
+      room = Room.new
+      room.save
+      helper.create_message(room.id, 'message').should be_false
+    end
   end
-  it "メッセージを更新する" do
-    message = Message.new(:body => 'init')
-    message.save
-    helper.update_message(message.id, 'modified').should be_true
-    Message.find(message.id).body.should == 'modified'
+  context "メッセージを更新する" do
+    it "成功する" do
+      message = Message.new(:body => 'init')
+      message.save
+      helper.update_message(message.id, 'modified').should be_true
+      Message.find(message.id).body.should == 'modified'
+    end
+    it "失敗する" do
+      message = mock
+      message.stub(:save).and_return(false)
+      message.stub(:body=)
+      Message.stub(:find).and_return(message)
+      helper.update_message(0, 'modified').should be_false
+    end
   end
-  it "メッセージを削除する" do
-    message = Message.new(:body => 'init')
-    message.save
-    helper.delete_message(message.id).should be_true
-    Message.find(message.id).should be_nil
+  context "メッセージを削除する" do
+    it "成功する" do
+      message = Message.new(:body => 'init')
+      message.save
+      helper.delete_message(message.id).should be_true
+      Message.find(message.id).should be_nil
+    end
+    it "失敗する" do
+      message = mock
+      message.stub(:destroy).and_return(false)
+      Message.stub(:find).and_return(message)
+      helper.delete_message(0).should be_false
+    end
   end
 end

ef05753aab8155653da23b34144d2da4cbc9196f
suer <sue@nagoya-u.jp>
2011-02-16 00:25:50 +0900

    hook の spec を追加 refs #350

---
+++ spec/lib/asakusa_satellite/hook_spec.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe AsakusaSatellite::Hook::Listener do
+  it "Listenerを登録すると call_hook で呼び出せる" do
+    class TestListener1 < AsakusaSatellite::Hook::Listener 
+      def method(context)
+        "test1 "  + context[:message]
+      end
+    end
+    class TestListener2 < AsakusaSatellite::Hook::Listener 
+      def method(context)
+        "test2 "  + context[:message]
+      end
+    end
+    class TestController
+      include AsakusaSatellite::Hook::Helper
+    end
+    TestController.new.call_hook(:method, {:message => 'message'}).should =~ /test1 message/
+    TestController.new.call_hook(:method, {:message => 'message'}).should =~ /test2 message/
+  end
+end

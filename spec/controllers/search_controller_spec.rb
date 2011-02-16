573149ad276f4e9335752678f37947981b2d08cf
mallowlabs <mallowlabs@gmail.com>
2011-02-17 01:49:27 +0900

    Added: spec for search_controller refs #390

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -11,6 +11,10 @@ describe SearchController do
   end
 
   describe "検索時は" do
+    it "検索文字列が空なら検索ページにリダイレクト" do
+      get :search, {:search => {:message => ''}}
+      response.should redirect_to(:controller => 'search', :action => 'index')
+    end
     it "ヒットするメッセージがあれば検索される" do
       Message.select.each { |r| r.delete }
       Room.select.each { |r| r.delete }
@@ -37,6 +41,27 @@ describe SearchController do
       assigns[:results].count.should == 1
       assigns[:results][0][:messages].count.should == 0
     end
+    it "部屋番号を指定すると一致する部屋だけ返す" do
+      Message.select.each { |r| r.delete }
+      Room.select.each { |r| r.delete }
+      room = Room.new(:title => 'test')
+      room.save!
+      room_id = room.id
+      message = Message.new
+      message.body = 'テストメッセージ'
+      message.room = room
+      message.save!
+      room = Room.new(:title => 'test')
+      room.save!
+      message = Message.new
+      message.body = 'テストメッセージ'
+      message.room = room
+      message.save!
+      get :search, {:search => {:message => 'テスト'}, :room => {:id => room_id}}
+      assigns[:results].count.should == 1
+      assigns[:results][0][:messages].count.should == 1
+      assigns[:results][0][:room].id.should == room_id
+     end
   end
 
 end

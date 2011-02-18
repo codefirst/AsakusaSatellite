f1a9232decdebbcecf219553aba98bcc26348910
suer <sue@nagoya-u.jp>
2011-02-18 23:47:15 +0900

    復活の呪文ページでログインしていない場合のspecを追加 refs #338

--- spec/controllers/account_controller_spec.rb
+++ spec/controllers/account_controller_spec.rb
@@ -2,8 +2,8 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe AccountController do
 
-  describe "GET 'index'" do
-    it "should be successful" do
+  describe "/index へアクセスする" do
+    it "ログインしている場合は status success" do
       user = mock_model(User)
       user.stub!(:spell).and_return('')
       user.stub!(:spell=)
@@ -11,6 +11,12 @@ describe AccountController do
       User.stub!(:find).and_return(user)
       get 'index'
       response.should be_success
+    end
+
+    it "ログインしていない場合はトップページへリダイレクトする" do
+      session[:current_user_id] = nil
+      get 'index'
+      response.should redirect_to(:controller => 'chat', :action => 'index')
     end
   end
 

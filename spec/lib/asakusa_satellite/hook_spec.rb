4195625557a5e5f194339502d9ecea236cc35fb6
mzp <mzpppp@gmail.com>
2011-02-21 17:24:47 +0900

    view hookのテストを動作するように修正 refs #421

--- spec/lib/asakusa_satellite/hook_spec.rb
+++ spec/lib/asakusa_satellite/hook_spec.rb
@@ -1,21 +1,22 @@
+# -*- coding: utf-8 -*-
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe AsakusaSatellite::Hook::Listener do
   it "Listenerを登録すると call_hook で呼び出せる" do
-    class TestListener1 < AsakusaSatellite::Hook::Listener 
-      def method(context)
+    class TestListener1 < AsakusaSatellite::Hook::Listener
+      def foo(context)
         "test1 "  + context[:message]
       end
     end
-    class TestListener2 < AsakusaSatellite::Hook::Listener 
-      def method(context)
+    class TestListener2 < AsakusaSatellite::Hook::Listener
+      def foo(context)
         "test2 "  + context[:message]
       end
     end
     class TestController
       include AsakusaSatellite::Hook::Helper
     end
-    TestController.new.call_hook(:method, {:message => 'message'}).should =~ /test1 message/
-    TestController.new.call_hook(:method, {:message => 'message'}).should =~ /test2 message/
+    TestController.new.call_hook(:foo, {:message => 'message'}).should =~ /test1 message/
+    TestController.new.call_hook(:foo, {:message => 'message'}).should =~ /test2 message/
   end
 end

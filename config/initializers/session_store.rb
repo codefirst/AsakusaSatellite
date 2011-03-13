250eb82f54f46053e1c34ccd37fe2852d6a66002
mzp <mzpppp@gmail.com>
2011-03-13 13:40:44 +0900

    ブラウザ終了後もセッションが保持されるようにした refs #475

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-AsakusaSatellite::Application.config.session_store :cookie_store, :key => '_AsakusaSatellite_session'
+AsakusaSatellite::Application.config.session_store :cookie_store, :key => '_AsakusaSatellite_session', :expire_after => 1.month
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # AsakusaSatellite::Application.config.session_store :active_record_store
+

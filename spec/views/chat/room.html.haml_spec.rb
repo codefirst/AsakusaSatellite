ddc8d612f3d7a8f92016c46de5c089fd895abbd5
mzp <mzpppp@gmail.com>
2011-03-14 23:35:25 +0900

    設定ページを作ったので、部屋名のon-the-spot編集はなくした refs #485

--- spec/views/chat/room.html.haml_spec.rb
+++ spec/views/chat/room.html.haml_spec.rb
@@ -9,22 +9,5 @@ describe "chat/room.html.haml" do
     @messages = []
   end
 
-  it "オーナーはタイトルを修正できる" do
-    session[:current_user_id] = @owner.id 
-    render 
-    rendered.should =~ /on_the_spot_editing/
-  end
-  it "オーナーじゃないユーザはタイトルを修正できない" do
-    user = User.new
-    user.save
-    session[:current_user_id] = user.id 
-    render 
-    rendered.should_not =~ /on_the_spot_editing/
-  end
-  it "ログインしていないユーザはタイトルを修正できない" do
-    session[:current_user_id] = nil 
-    render 
-    rendered.should_not =~ /on_the_spot_editing/
-  end
 end
 

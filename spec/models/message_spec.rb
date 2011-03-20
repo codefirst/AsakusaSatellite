0260a1e56e302608d3175b57c8e3763f7719aaea
mzp <mzpppp@gmail.com>
2011-03-20 10:42:59 +0900

    Messageモデルのテストをリファクタリング refs #498

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,70 +2,61 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Message do
-
   before do
-    @body = 'これは本文です'
-    @room_name = 'テストの部屋'
-    @room = Room.new(:title => @room_name)
-    @room.save!
-    @user = User.new(:name => 'test user', :screen_name => 'test')
-    @user.save!
-    @message = Message.new(:body => @body, :user => @user)
-    @message.save!
-  end
-
-  it "本文を取得できる" do
-    @message.body.should == @body
-  end
-
-  it "部屋を外部参照する" do
-    @message.room = @room
-    @message.save!
-    @message.room.title.should == @room_name
-  end
-
-  it "Hashに変換できる" do
-    @message.to_hash.should == {
-        "name" => @message.user.name,
-        "created_at" => I18n.l(@message.created_at),
-        "profile_image_url" => @message.user.profile_image_url,
-        "html_body" => @message.body,
-        "body" => @message.body,
-        "attachment" => nil,
-        "id" => @message.id,
-        "screen_name" => @message.user.screen_name
-    }
-  end
-end
-
-describe Message, "部屋のと関連" do
-  before do
-    @room_name = 'テストの部屋'
-    @room = Room.new(:title => @room_name)
-    @room.save!
-    @user = User.new(:name => 'test user', :screen_name => 'test')
-    @user.save!
+    @room = Room.new(:title => 'test room').tap{|x| x.save! }
+    @user = User.new(:name => 'test user', :screen_name => 'test').tap{|x| x.save! }
 
     @other_room = Room.new(:title => "dummy_room")
     @other_room.save!
 
     @messages = (0..10).map do|i|
       Message.new(:body => "dummy", :user => @user, :room => @other_room).save!
-
-      message = Message.new(:body => i.to_s, :user => @user, :room => @room)
-      message.save!
-      message
+      Message.new(:body => "body of message #{i}",
+                    :user => @user,
+                    :room => @room).tap{|m| m.save! }
     end
-    @target = @messages[5]
+
+    @message = @messages[5]
   end
 
-  it "自分より前のメッセージを取得できる" do
-    @target.prev(1).should == [ @messages[4] ]
-    @target.prev(2).should == [ @messages[3], @messages[4] ]
+  subject { @message }
+  its(:body) { should == @message.body }
+  its(:room) { should == @room }
+
+  describe "room" do
+    subject { @message.room }
+    its(:title) { should == @room.title }
   end
 
-  it "自分より後のメッセージを取得できる" do
-    @target.next(1).should == [ @messages[6] ]
-    @target.next(2).should == [ @messages[6], @messages[7] ]
+  describe "hash" do
+    before do
+      I18n.stub(:l){|s| "i18n:#{s}" }
+      AsakusaSatellite::Filter.stub(:process){|s| "filtered:#{s}" }
+    end
+
+    subject { @message.to_hash }
+    its(['name']) { should == @message.user.name }
+    its(['profile_image_url']) { should == @message.user.profile_image_url }
+    its(['body']) { should == @message.body }
+    its(['attachment']) { should == nil }
+    it { subject['id'].should == @message.id }
+    its(['screen_name']) { should == @message.user.screen_name }
+    its(['created_at']) { should == "i18n:#{@message.created_at}" }
+    its(['html_body']) { should == "filtered:#{@message.body}" }
+  end
+
+  it { should have(1).prev(1) }
+  it { should have(2).prev(2) }
+  it { should have(1).next(1) }
+  it { should have(2).next(2) }
+
+  describe "prev" do
+    subject { @message.prev 2 }
+    it { should == [ @messages[3], @messages[4] ] }
+  end
+
+  describe "next" do
+    subject { @message.next 2 }
+    it { should == [ @messages[6], @messages[7] ] }
   end
 end

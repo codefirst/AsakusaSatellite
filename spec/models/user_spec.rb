724b81819c6db5ddf7c4019435500ab86cffe9c5
mzp <mzpppp@gmail.com>
2011-03-20 09:30:06 +0900

    Userモデルのテストをリファクタリング refs #498

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,30 @@
+# -*- coding: utf-8 -*-
+
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe User do
-  it "フィールドを持つ" do
-    user = User.new
-    user.respond_to?(:spell).should be_true
-    user.respond_to?(:profile_image_url).should be_true
-    user.respond_to?(:screen_name).should be_true
-    user.respond_to?(:name).should be_true
-    user.respond_to?(:email).should be_true
-    user.respond_to?(:id).should be_true
+  before  do
+    @user = User.new(:name => 'test user',
+                     :screen_name => 'test',
+                     :email => 'user@example.com',
+                     :profile_image_url => 'http://example.com/profile.png',
+                     :spell => 'spell')
+  end
+
+  subject { @user }
+
+  it { should be_respond_to :name }
+  it { should be_respond_to :screen_name }
+  it { should be_respond_to :email }
+  it { should be_respond_to :profile_image_url }
+  it { should be_respond_to :spell }
+
+  describe "to_json" do
+    subject { @user.to_json }
+    its([:name]) { should == "test user" }
+    its([:screen_name]) { should == "test" }
+    its([:profile_image_url]) { should == "http://example.com/profile.png" }
+    it { should_not have_key(:spell) }
+    it { should_not have_key(:email) }
   end
 end

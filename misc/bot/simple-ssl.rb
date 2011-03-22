3d0971c0a4f54b37e5de3af382a18eafc24f2393
mallowlabs <mallowlabs@gmail.com>
2011-03-22 22:06:57 +0900

    Fixed: old bot samples refs #495

--- misc/bot/simple-ssl.rb
+++ misc/bot/simple-ssl.rb
@@ -5,11 +5,8 @@
 # example for bot
 # ------------------------------
 
-# Twitter's screen name
-ScreenName = "{screen name}"
-
 # Get from http://$AS_ROOT/account/index
-Password   = "{password}"
+ApiKey   = "YOUR_API_KEY"
 
 # EntryPoint
 EntryPoint = "https://localhost:3000/api/v1"
@@ -29,12 +26,7 @@ https = Net::HTTP.new(uri.host, uri.port)
 https.use_ssl = true
 https.verify_mode = OpenSSL::SSL::VERIFY_NONE
 https.start do| conn |
-  # login
-  session = conn.get(uri.path + "/login?user=#{ScreenName}&password=#{Password}")
-  cookie  = session['set-cookie'].split(";",2).first
-
   # post message
   p conn.post(uri.path + "/message",
-              "room_id=#{room_id}&message=#{message}",
-              "Cookie" => cookie)
+              "room_id=#{room_id}&message=#{message}&api_key=#{ApiKey}")
 end
--- misc/bot/simple.rb
+++ misc/bot/simple.rb
@@ -5,11 +5,8 @@
 # example for bot
 # ------------------------------
 
-# Twitter's screen name
-ScreenName = "AsaxaSatellite"
-
 # Get from http://$AS_ROOT/account/index
-Password   = "AsGklaaVMtMiVxx69OEsZfcma"
+ApiKey   = "YOUR_API_KEY"
 
 # EntryPoint
 EntryPoint = "http://localhost:3000/api/v1"
@@ -26,12 +23,7 @@ room_id, message = *ARGV
 uri = URI(EntryPoint)
 
 Net::HTTP.start(uri.host, uri.port) do| http |
-  # login
-  session = http.get(uri.path + "/login?user=#{ScreenName}&password=#{Password}")
-  cookie  = session['set-cookie'].split(";",2).first
-
   # post message
   p http.post(uri.path + "/message",
-              "room_id=#{room_id}&message=#{message}",
-              "Cookie" => cookie)
+              "room_id=#{room_id}&message=#{message}&api_key=#{ApiKey}")
 end

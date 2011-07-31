7c8d90a8649c5e8c7e847cf904f40b01fd2eeafe
suer <sue@nagoya-u.jp>
2011-07-31 15:39:10 +0900

    time zone refs #689

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module AsakusaSatellite
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Tokyo'
+    Time.zone = 'Asia/Tokyo'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]

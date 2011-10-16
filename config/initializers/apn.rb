cee6555fe3bc44d3f3c706a104ec04749e7d027b
mallowlabs <mallowlabs@gmail.com>
2011-10-16 23:43:48 +0900

    APN uses ENV['PEM'] refs #812

--- config/initializers/apn.rb
+++ config/initializers/apn.rb
@@ -1,7 +1,22 @@
 #! /opt/local/bin/ruby -w
 # -*- mode:ruby; coding:utf-8 -*-
+require 'base64'
 
 #APNS.host = 'gateway.push.apple.com'
-APNS.pem  = '/Users/mzp/Downloads/apns-dev-cert.pem'
+PEM_FILE = File.dirname(__FILE__) + '/../../tmp/apns-sandbox-cert.pem'
+
+pem = ENV['PEM']
+if pem
+   content = Base64.decode64(pem)
+else
+   warn "Set ENV['PEM'] for Notification"
+   content = ''
+end
+
+open(PEM_FILE, 'w') do |f|
+   f.write(content)
+end
+
+APNS.pem = PEM_FILE
 APNS.port = 2195
 

b1a7150fa6a80c5b85d326cbd0b14f72b4d442f7
mzp <mzpppp@gmail.com>
2011-10-26 22:21:54 +0900

    add QuoteIt( http://quoteit.heroku.com ) plugin refs #830

---
+++ vendor/plugins/as_quote_it/lib/quote_it.rb
@@ -0,0 +1,17 @@
+require 'open-uri'
+require 'nokogiri'
+require 'cgi'
+
+class AsakusaSatellite::Filter::QuoteIt < AsakusaSatellite::Filter::Base
+ def process(text, opts={})
+   root = opts[:root] || 'http://quoteit.heroku.com'
+   text.gsub URI.regexp(%w[http https]) do|url|
+      begin
+        open("#{root}/clip.html?u=#{CGI.escape url}").read
+      rescue
+        "<a href='#{url}'>#{url}</a>"
+      end
+    end
+ end
+end
+

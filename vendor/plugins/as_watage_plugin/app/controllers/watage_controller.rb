146fd32f1bafca6508af7451dd67aaac40ef2d56
SHIMOMURA Sho <graueule@gmail.com>
2012-08-11 13:21:05 +0900

    remove mime type refs #1259

--- vendor/plugins/as_watage_plugin/app/controllers/watage_controller.rb
+++ vendor/plugins/as_watage_plugin/app/controllers/watage_controller.rb
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 class WatageController < ApplicationController
   def show
-    name = params[:id]
-    format = params[:format]
+    filename = params[:id] + params[:format]
+    store_path = "#{Rails.root}/#{Setting[:attachment_path]}/#{filename}"
 
-    # todo: set correct mimetype
-    File.open("#{Rails.root}/#{Setting[:attachment_path]}/#{name}.#{format}", "rb") do |file|
-      send_data(file.read, :type => 'image/jpeg', :disposition=>'inline')
+    File.open(store_path, "rb") do |f|
+      send_data(f.read, :disposition=>'inline')
     end
   end
 end

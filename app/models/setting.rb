3e147f3cf75203e1ebcdb31abdde6fbc004b4a12
mallowlabs <mallowlabs@gmail.com>
2012-08-13 23:56:01 +0900

    Fixed: attachments settings configuration refs #1264

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,5 +1,5 @@
 class Setting
-  @@available_settings = YAML.load(File.open("#{Rails.root}/config/settings.yml"))
+  @@available_settings = YAML::load(ERB.new(File.read("#{Rails.root}/config/settings.yml")).result)
   def self.[](key)
     @@available_settings[key.to_s]
   end

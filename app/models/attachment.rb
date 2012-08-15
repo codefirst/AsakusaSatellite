b1be496662d323b64183cbfcb31ebb28e9227e9c
SHIMOMURA Sho <graueule@gmail.com>
2012-08-15 16:16:09 +0900

    support file attachment on watage refs #1279

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 require 'uuidtools'
+require 'net/https'
+require 'uri'
+require 'json'
 class Attachment
   UPLOAD_DIR = "#{Rails.root}/#{Setting[:attachment_path]}"
 
@@ -21,13 +24,22 @@ class Attachment
   end
 
   def self.create_and_save_file(filename, file, mimetype, message)
-    disk_filename = unique_id + "-" + filename
-    open("#{UPLOAD_DIR}/#{disk_filename}", "wb") do |f|
-      f.write(file.read)
+    filepath = ""
+
+    if Setting[:attachment_path].start_with? "http"
+      uri = URI.parse Setting[:attachment_path]
+      response = JSON.parse(post_multipart(uri, file).body)
+      filepath = response["source"]
+    else
+      filepath = "#{UPLOAD_DIR}/#{unique_id}-#{filename}"
+      open(filepath, "wb") do |f|
+        f.write(file.read)
+      end
     end
+
     attachment = Attachment.new(:filename => filename,
                                 :mimetype => mimetype,
-                                :disk_filename => "#{UPLOAD_DIR}/#{disk_filename}",
+                                :disk_filename => filepath,
                                 :message => message)
     attachment.save
     attachment
@@ -36,4 +48,24 @@ class Attachment
   def self.unique_id
     UUIDTools::UUID.random_create.to_s
   end
+
+  def self.post_multipart(uri, file, content_type=file.content_type, boundary="A300x")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.start do
+      request = Net::HTTP::Post.new(uri.path + "?" + (uri.query || ""))
+      request["user-agent"] = "Ruby/#{RUBY_VERSION} MyHttpClient"
+      request.set_content_type "multipart/form-data; boundary=#{boundary}"
+      body = "--#{boundary}\r\n"
+      body += "Content-Disposition: form-data; name=\"upload[file]\"; filename=\"#{URI.encode(file.original_filename)}\"\r\n"
+      body += "Content-Type: #{content_type}\r\n"
+      body += "\r\n"
+      body += "#{file.read}\r\n"
+      body += "\r\n"
+      body += "--#{boundary}--\r\n"
+      request.body = body
+      http.request request
+    end
+  end
+
 end

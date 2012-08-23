5049ed91fc3e9b6241db121596cc4db34dc9b646
mallowlabs <mallowlabs@gmail.com>
2012-08-23 21:59:25 +0900

    Fixed: regression refs #1264

--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -4,7 +4,7 @@ require 'yaml'
 
 describe Setting do
   before do
-    @settings = YAML.load(File.open("#{Rails.root}/config/settings.yml"))
+    @settings = YAML.load(ERB.new(File.open("#{Rails.root}/config/settings.yml").read).result)
   end
 
   it "各設定を読める" do

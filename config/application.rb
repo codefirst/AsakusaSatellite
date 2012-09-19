2d37eb8b76bf87b8b48f150c67245c34a42de767
mallowlabs <mallowlabs@gmail.com>
2012-09-19 21:00:06 +0900

    ReFixed: assets:precompile failed refs #1324

--- config/application.rb
+++ config/application.rb
@@ -62,5 +62,8 @@ module AsakusaSatellite
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # avoid assets:precompile failure on Heroku
+    config.assets.initialize_on_precompile = false
   end
 end

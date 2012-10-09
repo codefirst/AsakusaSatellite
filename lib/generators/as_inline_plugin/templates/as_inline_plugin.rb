8c4772e5a85dfe7ecdde44d7dbf2bc1532d872be
mallowlabs <mallowlabs@gmail.com>
2012-10-09 21:41:51 +0900

    Removed: old generator refs #1332

--- lib/generators/as_inline_plugin/templates/as_inline_plugin.rb
+++
@@ -1,9 +0,0 @@
-require 'asakusa_satellite/filter/inline_plugin'
-
-class <%= class_name %>Filter < AsakusaSatellite::Filter::InlinePlugin
-  plugin :test do|a,b|
-    # generated method
-    # modify here
-  end
-end
-

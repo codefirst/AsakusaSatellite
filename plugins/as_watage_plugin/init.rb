9fa3fc8336cbdba89ade2ec10f953a15888a1ec1
SHIMOMURA Sho <graueule@gmail.com>
2012-10-31 02:13:48 +0900

    send access_token to watage refs #1363

--- plugins/as_watage_plugin/init.rb
+++ plugins/as_watage_plugin/init.rb
@@ -5,8 +5,11 @@ require 'rest-client'
 module Watage
   class WatagePolicy
     def upload(filename, file, mimetype, message)
-      uri = URI.parse Setting[:attachment_path]
-      response_str = RestClient.post uri.to_s, { "upload[file]" => file }
+      uri = URI.parse("#{Setting[:attachment_path]}/api/v1/put")
+      response_str = RestClient.post(uri.to_s, {
+                       "upload[file]" => file,
+                       "access_token" => Setting[:watage_token],
+                       "access_token_secret" => Setting[:watage_token_secret]})
       response = JSON.parse response_str
       response["source"]
     end

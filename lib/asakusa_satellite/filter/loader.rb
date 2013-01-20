bf87c207e5ab520fc52d724072af24861229b31a
mallowlabs <mallowlabs@gmail.com>
2013-01-20 21:54:53 +0900

    Refactored: extract filter loader refs #1410

---
+++ lib/asakusa_satellite/filter/loader.rb
@@ -0,0 +1,41 @@
+module AsakusaSatellite
+  module Filter
+    class Loader
+
+      def self.load
+        $: << "#{File.dirname(__FILE__)}/../../.."
+        AsakusaSatellite::Filter::FilterConfig.plugins_dirs.map { |dir|
+          File.join("plugins", dir)
+        }.each do |dir|
+          next unless File.directory?(dir)
+
+          lib = File.join(dir, "lib")
+          if File.directory?(lib)
+            $:.unshift lib
+            ActiveSupport::Dependencies.autoload_paths += [lib]
+          end
+
+          initrb = File.join(dir, "init.rb")
+          require initrb if File.file?(initrb)
+
+          locales = Dir.glob(File.join(dir, 'config', 'locales', '*.yml'))
+          ::I18n.load_path += locales
+
+          views = File.join(dir, "app", "views")
+          ActionController::Base.prepend_view_path(views) if File.directory?(views)
+
+          %w(models controllers helpers).each do |dirname|
+            subdir = File.join(dir, "app", dirname)
+            ActiveSupport::Dependencies.autoload_paths += [subdir] if File.directory?(subdir)
+          end
+
+          initializers = Dir.glob(File.join(dir, 'config', 'initializers', '*.rb'))
+          initializers.each do |initializer|
+            require initializer
+          end
+
+        end
+      end
+    end
+  end
+end

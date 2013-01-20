8095599e4c8599f026205bee1a263c2c364521c1
SHIMOMURA Sho <graueule@gmail.com>
2013-01-20 16:44:05 +0900

    fix typo refs #1411

--- lib/generators/as_viewhook/templates/as_viewhook.rb
+++ lib/generators/as_viewhook/templates/as_viewhook.rb
@@ -4,7 +4,7 @@ class <%= class_name %>Listener < AsakusaSatellite::Hook::Listener
 
   # or define method by yourself
   def hookname(context)
-    context[:controller].send(:render_to_string, {:locals => context, :pertial => '_view_file'})
+    context[:controller].send(:render_to_string, {:locals => context, :partial => '_view_file'})
   end
 end
 

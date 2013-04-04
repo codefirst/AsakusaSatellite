0922a6e39a5bf094aa4a315c2c8bc2494651d790
SHIMOMURA Sho <graueule@gmail.com>
2013-04-04 20:27:55 +0900

    flexible omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,13 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if Setting['omniauth']['provider_args'].nil? or Setting['omniauth']['provider_args'].empty?
     provider Setting['omniauth']['provider'].to_sym
   else
-    provider Setting['omniauth']['provider'].to_sym, *Setting['omniauth']['provider_args']
+    if Setting['omniauth']['provider_args'].class == Hash
+      args = {}
+      Setting['omniauth']['provider_args'].map {|k,v| args[k.to_sym] = v}
+      provider Setting['omniauth']['provider'].to_sym, args
+    else
+      provider Setting['omniauth']['provider'].to_sym, *Setting['omniauth']['provider_args']
+    end
   end
 end
 

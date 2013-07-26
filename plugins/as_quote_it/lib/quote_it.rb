8f2879e14e41c68aef609684e06f2745b21aa7d8
mzp <mzpppp@gmail.com>
2013-07-26 08:46:26 +0400

    Use ENV['QUOTEIT_URL'] as quoteit url refs #120

--- plugins/as_quote_it/lib/quote_it.rb
+++ plugins/as_quote_it/lib/quote_it.rb
@@ -3,7 +3,7 @@ require 'cgi'
 
 class AsakusaSatellite::Filter::QuoteIt < AsakusaSatellite::Filter::Base
  def process(text, opts={})
-   root = opts[:root] || 'http://quoteit.heroku.com'
+   root = opts[:root] || ENV['QUOTEIT_URL'] || 'http://quoteit.heroku.com'
    text.gsub URI.regexp(%w[http https]) do|url|
       begin
         open("#{root}/clip.html?u=#{CGI.escape url}").read

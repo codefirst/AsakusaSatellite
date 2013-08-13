7ca0e2dfd645f4319661701dd06dfc5b3e621a8b
banjun <banjun@gmail.com>
2013-08-13 23:00:41 +0900

    set http status code to api results whose status = error refs #126

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -2,12 +2,13 @@
 module Api
   module V1
     class LoginController < ApplicationController
+      include ApiHelper
       respond_to :json
       def index
         ActiveSupport::Deprecation.warn "Api::V1::LoginController is deprecated. Instead, please use api_key parameter"
         users = User.where(:screen_name => params[:user], :spell => params[:password])
         if users.size == 0
-          render :json => {:status => 'error', :error => 'login failed'}
+          render_error 'login failed', 403
           return
         end
         session[:current_user_id] = users.first.id

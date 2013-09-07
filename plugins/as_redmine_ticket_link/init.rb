4e94aa34752eda7d13993b89bd61a232b95b7d81
suer <suetsugu.r@gmail.com>
2013-09-07 23:23:47 +0900

    Revert "Fixed: as_redmine_ticket_link_filter does not work refs #130"
    This reverts commit 63e3a3484dffa8bf7990ba419c366377a8870e64.

--- plugins/as_redmine_ticket_link/init.rb
+++ plugins/as_redmine_ticket_link/init.rb
@@ -1,6 +1,6 @@
 require 'asakusa_satellite/config'
 
-require 'redmine_ticket_link'
+require 'redmine_ticket_link_filter'
 require 'redmine_ticket_link_hook'
 AsakusaSatellite::Config.room("Redmine Ticket Link",
                               :controller=>:as_redmine_ticket_link, :action=> :room)

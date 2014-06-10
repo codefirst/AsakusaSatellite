b52fb6f55583725e9ccea4f1f3572573cef81e8d
Ryo SUETSUGU <suetsugu.r@gmail.com>
2014-06-10 21:49:09 +0900

    refactor refs #189

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -33,11 +33,7 @@ class LoginController < ApplicationController
       session.delete :cached_current_user
       session.delete :current_user_id
     end
-    if request.referer
-      redirect_to request.referer
-    else
-      redirect_to root_path
-    end
+    redirect_to(request.referer || root_path)
   end
 
   def failure

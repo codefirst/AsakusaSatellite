ea6c77f62f2a6528813a732854d2c92fdab2903a
mallowlabs <mallowlabs@gmail.com>
2014-06-24 23:44:03 +0900

    Added: callbacks to device model refs #191

--- app/models/device.rb
+++ app/models/device.rb
@@ -5,4 +5,24 @@ class Device
   field :device_name
   field :device_type
   embedded_in :User, :inverse_of => :devices
+
+  @@after_save_procs = []
+  def self.add_after_save(proc)
+    @@after_save_procs << proc
+  end
+  after_save do |device|
+    @@after_save_procs.each do |proc|
+      proc.call(device)
+    end
+  end
+
+  @@after_destory_procs = []
+  def self.add_after_destroy(proc)
+    @@after_destory_procs << proc
+  end
+  after_destroy do |device|
+    @@after_destory_procs.each do |proc|
+      proc.call(device)
+    end
+  end
 end

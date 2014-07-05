98bbef90ee277295aca1cf3471cf37e3b97a94f9
mallowlabs <mallowlabs@gmail.com>
2014-07-05 22:24:39 +0900

    Added: PushIt support refs #196
    https://github.com/codefirst/push-it/

--- plugins/as_iphone_notifier/init.rb
+++ plugins/as_iphone_notifier/init.rb
@@ -2,4 +2,5 @@ require 'as_iphone_notifier'
 require 'asakusa_satellite/apn_service/base'
 require 'asakusa_satellite/apn_service/embed'
 require 'asakusa_satellite/apn_service/zero_push'
+require 'asakusa_satellite/apn_service/push_it'
 
---
+++ plugins/as_iphone_notifier/lib/asakusa_satellite/apn_service/push_it.rb
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+
+module AsakusaSatellite
+  module APNService
+    class PushIt < Base
+      def initialize
+        uri = URI.parse(ENV['PUSH_IT_URL'])
+        @client = Net::HTTP.new(uri.host, uri.port)
+        @client.use_ssl = uri.scheme == 'https'
+      end
+
+      def send_message(device_tokens, room, text)
+        body = json(device_tokens, room, text).to_json
+        begin
+          @client.start do |connection|
+            connection.post("/message", body)
+          end
+        rescue Errno::ECONNREFUSED => e
+          Rails.logger.error e
+        end
+      end
+
+      private
+
+      def json(device_tokens, room, text)
+        {
+          :tokens => device_tokens,
+          :payload => {
+            :aps => {
+              :alert => text,
+              :sound => "default"
+            },
+            :id => room.id
+          }
+        }
+      end
+
+    end if ENV['PUSH_IT_URL']
+  end
+end

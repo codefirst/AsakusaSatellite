448dee4a7396137f8826fe292fcdb5ee64883f35
mallowlabs <mallowlabs@gmail.com>
2014-12-30 14:07:56 +0900

    Fixed: quote_it_plugin could not handle & refs #208

--- plugins/as_quote_it/lib/quote_it.rb
+++ plugins/as_quote_it/lib/quote_it.rb
@@ -8,11 +8,16 @@ class AsakusaSatellite::Filter::QuoteIt < AsakusaSatellite::Filter::Base
     root = opts[:root] || ENV['QUOTEIT_URL'] || 'https://quoteit.herokuapp.com'
     text.gsub REGEXP do |url|
       begin
-        open("#{root}/clip.html?u=#{CGI.escape url}").read
+        open("#{root}/clip.html?u=#{to_param(url)}").read
       rescue
         %[<a target="_blank" href="#{url}">#{url}</a>]
       end
     end
+  end
+
+  private
+  def to_param(url)
+    CGI.escape(REXML::Text.unnormalize(url))
   end
 end
 

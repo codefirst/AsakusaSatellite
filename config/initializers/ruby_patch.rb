13c24e1a10f5cc8362ff8e908f391c791772c333
mallowlabs <mallowlabs@gmail.com>
2015-02-12 01:21:33 +0900

    Removed: warning: Comparable#== will no more rescue exceptions of #<=> in the next release. refs #211

--- config/initializers/ruby_patch.rb
+++ config/initializers/ruby_patch.rb
@@ -6,3 +6,14 @@ class Array
   end
 end
 
+# for Ruby 2.2.0
+# https://github.com/rails/rails/commit/b0acc77edced44e47c8570bf7dddd4ce19f06cb0
+class DateTime
+  def <=>(other)
+    if other.respond_to? :to_datetime
+      super other.to_datetime
+    else
+      nil
+    end
+  end
+end

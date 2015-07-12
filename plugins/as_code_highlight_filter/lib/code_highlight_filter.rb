7b31f9becd6a5d9ec7ebe41163a08c07e91bfd47
mallowlabs <mallowlabs@gmail.com>
2015-07-12 11:22:34 +0900

    Changed: good-bye CodeRay and hello Rouge refs #222

--- plugins/as_code_highlight_filter/lib/code_highlight_filter.rb
+++ plugins/as_code_highlight_filter/lib/code_highlight_filter.rb
@@ -4,11 +4,14 @@ class CodeHighlightFilter < AsakusaSatellite::Filter::Base
   def process_all(lines, opts={})
     lang,*body = lines
     content = REXML::Text::unnormalize(body.join("\n"))
-    case lang
+    case lang.strip
     when "graphviz::","graph::"
-      %(<img class="graphviz" src="http://chart.googleapis.com/chart?cht=gv&amp;chl=#{CGI.escape content}" />)
+      %(<img class="graphviz" src="https://chart.googleapis.com/chart?cht=gv&amp;chl=#{CGI.escape content}" />)
     when /\A(\w+)::\Z/
-      [CodeRay.scan(content, $1).div]
+      lexer_class = Rouge::Lexer.find($1)
+      lexer = lexer_class ? lexer_class.new : Rouge::Lexers::PlainText.new
+      formatter = Rouge::Formatters::HTML.new(:css_class => 'highlight', :inline_theme => 'github')
+      [formatter.format(lexer.lex(content))]
     else
       lines
     end

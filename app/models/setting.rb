e16bc2cfbc46de587f60a0e5e17391347356e695
knothole <knotholex@gmail.com>
2019-10-20 11:17:24 +0900

    Add customizable settings file

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,5 +1,13 @@
 class Setting
-  @@available_settings = YAML::load(ERB.new(File.read("#{Rails.root}/config/settings.yml")).result)
+  class << self
+    def settings_path
+      name = "#{ENV['SETTINGS'] || 'settings'}.yml"
+      Rails.root.join('config', name)
+    end
+  end
+
+  @@available_settings = YAML::load(ERB.new(File.read(settings_path)).result)
+
   def self.[](key)
     @@available_settings[key.to_s]
   end

39a0f589f3795590620bef0aae53657369365332
SHIMOMURA Sho <graueule@gmail.com>
2019-12-14 02:23:26 +0900

    Allow CORS for API

---
+++ config/initializers/cors.rb
@@ -0,0 +1,9 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "*"
+
+    resource "/api/*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
